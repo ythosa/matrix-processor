aa3f58265ff976faec7eed558b124ec812a8d66b
ythosa <vasus714@yandex.ru>
2020-07-30 18:52:45 +0300

    Update user interface. Add help and select functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,42 @@
 package processor
 
 import java.util.*
+import kotlin.system.exitProcess
 
 fun main() {
     val scan = Scanner(System.`in`)
-    multiplicationMatrixByNumber(scan)
+    printInfo()
+    selectAction(scan)
 }
 
-fun multiplicationMatrixByNumber(scan: Scanner) {
-    val mat = readMatrix(scan)
+fun selectAction(scan: Scanner) {
+    when (scan.nextLine().toInt()) {
+        1 -> addMatrices(scan)
+        2 -> multiplicationMatrixToConstant(scan)
+        0 -> exitProcess(1)
+    }
+}
+
+fun printInfo() {
+    print("1. Add matrices\n" +
+            "2. Multiply matrix to a constant\n" +
+            "3. Multiply matrices\n" +
+            "0. Exit\n" +
+            "Your choice: ")
+}
+
+fun multiplicationMatrixToConstant(scan: Scanner) {
+    val (rows, cols) = readMatrixSize(scan, "")
+    val mat = readMatrix(scan, rows, cols)
     val whatTimes = scan.nextInt()
     println(mat * whatTimes)
 }
 
-fun sumMatrices(scan: Scanner) {
-    val mat1 = readMatrix(scan)
-    val mat2 = readMatrix(scan)
+fun addMatrices(scan: Scanner) {
+    val (rows1, cols1) = readMatrixSize(scan, "first ")
+    val mat1 = readMatrix(scan, rows1, cols1)
+    val (rows2, cols2) = readMatrixSize(scan, "second ")
+    val mat2 = readMatrix(scan, rows2, cols2)
     if (mat1.rows != mat2.rows || mat1.columns != mat2.columns) {
         println("ERROR")
     } else {
@@ -23,9 +44,14 @@ fun sumMatrices(scan: Scanner) {
     }
 }
 
-fun readMatrix(scan: Scanner): Matrix {
+fun readMatrixSize(scan: Scanner, n: String): Pair<Int, Int> {
+    print("Enter the size of ${n}matrix: ")
     val dim = scan.nextLine().trim().split(" ").map(String::toInt)
-    val result = Matrix(dim[0], dim[1])
+    return Pair(dim[0], dim[1])
+}
+
+fun readMatrix(scan: Scanner, rows: Int, cols: Int): Matrix {
+    val result = Matrix(rows, cols)
     for (i in 0 until result.rows) {
         val row = scan.nextLine().trim().split(" ").map(String::toInt).toIntArray()
         result.fillLine(i, row)
