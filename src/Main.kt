3e31d5ac89ede2239f3d4731e074d35493fb10d9
ythosa <vasus714@yandex.ru>
2020-07-30 19:01:21 +0300

    Update user interface

--- src/Main.kt
+++ src/Main.kt
@@ -27,19 +27,21 @@ fun printInfo() {
 
 fun multiplicationMatrixToConstant(scan: Scanner) {
     val (rows, cols) = readMatrixSize(scan, "")
-    val mat = readMatrix(scan, rows, cols)
+    val mat = readMatrix(scan, "", rows, cols)
     val whatTimes = scan.nextInt()
+    println("The multiplication result is:")
     println(mat * whatTimes)
 }
 
 fun addMatrices(scan: Scanner) {
     val (rows1, cols1) = readMatrixSize(scan, "first ")
-    val mat1 = readMatrix(scan, rows1, cols1)
+    val mat1 = readMatrix(scan, "first ", rows1, cols1)
     val (rows2, cols2) = readMatrixSize(scan, "second ")
-    val mat2 = readMatrix(scan, rows2, cols2)
+    val mat2 = readMatrix(scan, "second ", rows2, cols2)
     if (mat1.rows != mat2.rows || mat1.columns != mat2.columns) {
         println("ERROR")
     } else {
+        println("The result is: ")
         println(mat1 + mat2)
     }
 }
@@ -50,7 +52,8 @@ fun readMatrixSize(scan: Scanner, n: String): Pair<Int, Int> {
     return Pair(dim[0], dim[1])
 }
 
-fun readMatrix(scan: Scanner, rows: Int, cols: Int): Matrix {
+fun readMatrix(scan: Scanner, n: String, rows: Int, cols: Int): Matrix {
+    println("Enter {$n}matrix")
     val result = Matrix(rows, cols)
     for (i in 0 until result.rows) {
         val row = scan.nextLine().trim().split(" ").map(String::toInt).toIntArray()
